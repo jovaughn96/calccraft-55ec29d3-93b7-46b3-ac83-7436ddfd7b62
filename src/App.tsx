c2bf817084c4fca3bb3b36f0dff8431506075b90
jovaughn96 <jovaughna.stephens@gmail.com>
2025-03-01 04:47:52 -0500

    Manual Code Modification: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,1 @@
-import Calculator from './components/Calculator';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <Calculator />
-    </div>
-  );
-}
-
-export default App;
+i
